6bc2d4b0393238df0280821ed80ce0c564c0c746
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-16 03:13:31 +0300

    3.3. Разбор первой программы. [#288346]

---
+++ src/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Ruslan Popov");
+        System.out.println("29.07.1987");
+    }
+}
