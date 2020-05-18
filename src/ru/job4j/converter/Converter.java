2c631d199079a2ed0b30f79e2e91da172fdb0e12
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-19 02:06:42 +0300

    5. Конвертер валюты [#288326]

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,22 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static double rubToEur(double x) {
+        double y = x/70;
+        return y;
+    }
+
+    public static double rubToUsd(double x) {
+        double y = x/60;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        double rubSum = 130.52;
+        double resRubToEur = Converter.rubToEur(rubSum);
+        double resRubToUsd = Converter.rubToUsd(rubSum);
+        System.out.println(rubSum + " rubles are " + resRubToEur + " euro");
+        System.out.println(rubSum + " rubles are " + resRubToUsd + " US dollars");
+    }
+}
