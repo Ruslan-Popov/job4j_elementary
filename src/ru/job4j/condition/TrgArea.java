f1a8921e11b662c168bb7be401facd4371c62866
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-21 00:54:26 +0300

    9.2. Checkstyle [#288341]

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -3,12 +3,12 @@ package ru.job4j.condition;
 public class TrgArea {
     public static double area(double a, double b, double c) {
         double p = (a + b + c) / 2;
-        double rsl = Math.sqrt( p * (p - a) * (p - b) * (p - c) );
+        double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return rsl;
     }
 
     public static void main(String[] args) {
-        double result = TrgArea.area(2,2,2);
+        double result = TrgArea.area(2, 2, 2);
         System.out.println("area (2,2,2) = " + result);
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,12 +3,12 @@ package ru.job4j.converter;
 public class Converter {
 
     public static double rubToEur(double x) {
-        double y = x/70;
+        double y = x / 70;
         return y;
     }
 
     public static double rubToUsd(double x) {
-        double y = x/60;
+        double y = x / 60;
         return y;
     }
 
