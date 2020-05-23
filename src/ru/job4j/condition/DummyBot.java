3f96ae7337df2966e00b7a9c07a2ca166281d27e
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-23 03:49:47 +0300

    4.1.2. Глупый бот. [#288287]

---
+++ src/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if (question.equals("Пока.")) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
