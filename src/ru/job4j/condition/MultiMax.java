c00fdf9543539e1b383c55507070a4a10d8cd9f7
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-25 23:51:19 +0300

    4.4. Максимум из трех чисел [#288296]

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,9 +2,10 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int a, int b, int c) {
-        boolean aMax = a > b && a > c;
-        boolean bMax = b > a && b > c;
-        int result = aMax ? a : bMax ? b : c;
+        boolean abMax = a > b;
+        int abResult = abMax ? a : b;
+        boolean cMax = c > abResult;
+        int result = cMax ? c : abResult;
         return result;
     }
 }
