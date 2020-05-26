2a712171b9475b6a81400fb78b452cb67c20c017
Ruslan-Popov <ruslan_popov@mail.ru>
2020-05-27 00:54:49 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#288384]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -14,4 +14,14 @@ public class Counter {
         }
         return rsl;
     }
+
+    public static int sumByEven(int start, int finish) {
+        int rsl = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                rsl += i;
+            }
+        }
+        return rsl;
+    }
 }
