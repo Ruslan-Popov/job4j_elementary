8fd04889a78758fcbf9a3314ba9dacdbef3b948e
Ruslan-Popov <ruslan_popov@mail.ru>
2020-07-01 01:09:39 +0300

    6.5. Поиск индекса в диапазоне. [#288308]

--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -14,10 +14,30 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whetArrayNotHas8ThenMinus1() {
+    public void whenArrayNotHas8ThenMinus1() {
         FindLoop find = new FindLoop();
         int[] arr = new int[]{3, 4, 15};
         int rst = find.indexOf(arr, 8);
+        assertThat(rst, is(-1));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] arr = {10, 4, 3, 8, 7};
+        int el = 8;
+        int start = 2;
+        int finish = 4;
+        int rst = FindLoop.indexOf(arr, el, start, finish);
+        assertThat(rst, is(3));
+    }
+
+    @Test
+    public void whenFind2() {
+        int[] arr = {18, 5, 2, 0, 8};
+        int el = 8;
+        int start = 1;
+        int finish = 2;
+        int rst = FindLoop.indexOf(arr, el, start, finish);
         assertThat(rst, is(-1));
     }
 }
