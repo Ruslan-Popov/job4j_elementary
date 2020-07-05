0ef70e66abb70657b5363fd029537579f5c13025
Ruslan-Popov <ruslan_popov@mail.ru>
2020-07-05 23:14:15 +0300

    6.7.4. Выигрышные комбинации в сокобан [#288305]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -33,8 +33,8 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
+        char[] diag = extractDiagonal(board);
         for (int i = 0; i < board.length; i++) {
-            char[] diag = extractDiagonal(board);
             if (diag[i] == 'X') {
                 if (monoHorizontal(board, i) || monoVertical(board, i)) {
                     result = true;
