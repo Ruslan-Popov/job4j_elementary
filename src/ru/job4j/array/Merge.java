234ae3c0c1e84a009799fb7b0a9490e237a81e98
Ruslan-Popov <ruslan_popov@mail.ru>
2020-09-09 01:51:51 +0300

    Упражнения 1.1.5. Массивы 3. Объединить два отсортированных массива

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -5,14 +5,23 @@ import ru.job4j.SortSelected;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
+        int l = 0;
+        int r = 0;
         for (int i = 0; i < rsl.length; i++) {
-            if (i < left.length) {
-                rsl[i] = left[i];
+            if (l > left.length - 1) {
+                rsl[i] = right[r];
+                r++;
+            } else if (r > right.length - 1) {
+                rsl[i] = left[l];
+                l++;
+            } else if (left[l] < right[r]) {
+                rsl[i] = left[l];
+                l++;
             } else {
-                rsl[i] = right[i - left.length];
+                rsl[i] = right[r];
+                r++;
             }
         }
-        SortSelected.sort(rsl);
         return rsl;
     }
 }
